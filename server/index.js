61a510b5be61eb193dd9e90caa3551eaaf4ae373
JayChandlani <jaychandlanijay@gmail.com>
2023-01-31 22:11:21 +0530

    bug fix

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
-// app.use(express.json());
+app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
