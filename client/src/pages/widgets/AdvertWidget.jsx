e0ca40bcb13869bd513d0b511a94e2d9529ee9ce
JayChandlani <jaychandlanijay@gmail.com>
2023-02-01 17:52:44 +0530

    server connected

--- client/src/pages/widgets/AdvertWidget.jsx
+++ client/src/pages/widgets/AdvertWidget.jsx
@@ -20,7 +20,7 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="http://localhost:3001/assets/advertisement.jpg"
+        src="https://socialgram-server.onrender.com/assets/advertisement.jpg"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
--- client/src/pages/widgets/FriendListWidget.jsx
+++ client/src/pages/widgets/FriendListWidget.jsx
@@ -14,7 +14,7 @@ const FriendListWidget = ({ userId }) => {
   const getFriends = async () => {
     if (friends.length) {
       const response = await fetch(
-        `http://localhost:3001/users/${userId}/friends`,
+        `https://socialgram-server.onrender.com/users/${userId}/friends`,
         {
           method: "GET",
           headers: { Authorization: `Bearer ${token}` },
